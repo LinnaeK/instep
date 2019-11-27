45bfe5ab7839f55a2509ca512f339fba566d0571
linnaek <linnaekraemer@gmail.com>
2019-11-27 12:11:46 -0600

    oauth error resolved

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,12 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var userCtrl = require('../controllers/users')
-var teachersCntrl = require('../controllers/teachers')
-var adminCntrl = require('../controllers/admin')
-var auth = require('../public/js/authenticate')
+// var teachersCntrl = require('../controllers/teachers')
+// var adminCntrl = require('../controllers/admin')
+// var auth = require('../public/js/authenticate')
 
 /* GET home page. */
 router.get('/', userCtrl.index);
-router.get('/loggedin/', teachersCntrl.index);
-router.get('/loggedin/', auth.isLoggedIn, auth.isAdmin, adminCntrl.index);
 
 router.get('/auth/google', passport.authenticate(
   'google',
